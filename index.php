47abdfcc81760851f35ecf57395e9188880911c9
Nignesh <NiGi@Nigneshs-MacBook-Pro.local>
2015-03-13 13:34:17 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?PHP
+?> 
+
+<!DOCTYPE HTML>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>Midterm - micro blog</title>
+    </head>
+    <body>        
+    Midterm
+    </body>
+</html>
+
+
